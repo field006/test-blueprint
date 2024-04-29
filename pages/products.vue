01296ff4c83d2996e6686fabc0023794e9fcb9f4
RyeConstantine <favianbasyari@gmail.com>
2024-04-29 10:58:03 +0700

    update revision 3

--- pages/products.vue
+++ pages/products.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <!-- Move the button above the table -->
-    <h1 class="mt-16" style="height: 1px;">Products List</h1>
+    <h1 style="height: 1px;">Products List</h1>
     <div class="action-buttons">
       <v-btn color="primary" dark @click="createProduct">Create Product</v-btn>
     </div>
